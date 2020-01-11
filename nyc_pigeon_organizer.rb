f8126a1329f784c0515fd113d645bb1feb645ddf
anam soomro <anamsoomroed@gmail.com>
2020-01-11 18:41:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  names = []
+  data.each { |attribute, attribute_data|
+    attribute_data.each { |category, category_data|
+      category_data.each { |x|
+        if !names.include? x
+        names << x
+        end
+      }
+    }
+  }
+  names.each do |name|
+    data.each do |attribute, attribute_data|
+      #binding.pry
+      selected_keys = data[attribute].select do |category, category_data| 
+        category_data.include? name
+      end
+      selected_array = selected_keys.map {|category, category_data| category.to_s}
+      if result[name]
+        result[name][attribute] = selected_array
+      else 
+        result[name] = {attribute => selected_array}
+      end
+    end
+  end
+  result
 end
