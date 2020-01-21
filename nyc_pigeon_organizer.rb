7e30aa53453e7f7e7a6883069ed20ed4de712878
anam soomro <anamsoomroed@gmail.com>
2020-01-21 18:01:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,5 @@
 require 'pry'
 
-
-
-
 def nyc_pigeon_organizer(data)
   result = {}
   names = []
@@ -17,7 +14,6 @@ def nyc_pigeon_organizer(data)
   }
   names.each do |name|
     data.each do |attribute, attribute_data|
-      #binding.pry
       selected_keys = data[attribute].select do |category, category_data| 
         category_data.include? name
       end
@@ -31,3 +27,33 @@ def nyc_pigeon_organizer(data)
   end
   result
 end
+
+
+#MUCHH SIMPLER WAY OF DOING IT
+
+=begin
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        new_hash[name][key] << new_value.to_s
+      end
+    end
+  end
+  new_hash
+end
+=end
+
+
+
+
+
+
+
